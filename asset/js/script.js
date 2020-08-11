8533a637262ad0791bed4bd1b79e2acb3eeda8d6
hanifrev <hanifrev@gmail.com>
2020-08-11 08:39:49 +0700

    pre webpack

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,7 +1,7 @@
 const header = document.querySelector('.header');
 header.style.backgroundColor = '#00205b';
 header.innerHTML = `<h1><a href="../index.html">FC United of Transilvania</a></h1>`;
-// header.innerHTML = ``
+
 
 // STICKY NAVBAR WHEN SCROLLED DOWN
 window.onscroll = function() { myFunction() };
@@ -39,7 +39,7 @@ template.innerHTML = `
         }
     }
 </style>
-<div class="welcome">
+<div class="">
     <p></p>
 </div>
 `;
@@ -57,6 +57,7 @@ class Welcome extends HTMLElement {
 
 customElements.define('welcome-greet', Welcome);
 
+
 class MainPagePhoto2 extends HTMLElement {
     constructor() {
         super();
@@ -68,6 +69,7 @@ class MainPagePhoto2 extends HTMLElement {
                 display: block;
                 margin-left: auto;
                 margin-right: auto;
+                padding: 20px;
                 width: 50%; }
         </style>
         <img src=${this.getAttribute("src")}>
@@ -78,18 +80,12 @@ class MainPagePhoto2 extends HTMLElement {
 customElements.define('main-page-photo2', MainPagePhoto2);
 
 
-// const template = document.createElement('template');
-// template.innerHTML =
 
 class SomeColumn extends HTMLElement {
     constructor() {
         super();
 
         this.attachShadow({ mode: 'open' });
-        // this.shadowRoot.appendChild(template.content.cloneNode(true));
-
-        // this.shadowRoot.querySelector('p').innerText = this.getAttribute('name');
-
         this.shadowRoot.innerHTML = `
         <style> .${this.getAttribute("class")} {
           
@@ -97,32 +93,78 @@ class SomeColumn extends HTMLElement {
 
         .first-col { 
             // grid-area: firstcol;
-            font-size: 15px;
-            color: red;
+            font-size: 16px;
             display: grid;
+            text-align: center;
+            margin: 20px;
+            line-height: 25px;
+            
         }
-
+        
         .second-col {
             // grid-area: secondcol;
-            font-size: 15px;
-            color: green;           
+            font-size: 16px;               
             display: grid;
+            text-align: justify;
+            margin: 20px;
+            line-height: 25px;
         }
 
         .third-col {
             // grid-area: thirdcol;
-            font-size: 15px;
+            font-size: 16px;         
+            text-align: justify;
+            margin: 20px;
+            line-height: 25px;
+        }
+
+        .first-col button {
+            width: 70%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-left: auto;
+            margin-right: auto;
+            border-radius: 15px 15px 15px 15px;
             color: blue;
+        }
+        .first-col img {
+            display: flex;
+            margin-left: auto;
+            margin-right: auto;
+            width: 100%;
+            padding: 20px 0px 20px 0px;
+        }
+        .first-col a {
+            text-decoration: none;
+        }
+        .first-col button:hover {
+            transform: scale(1.01);
+            box-shadow: 0px 0px 20px 5px rgba(81,183,209,0.6);
             
+        }
+        .second-col:hover {
+            transform: scale(1.05);
+            box-shadow: 0 0 40px -10px rgba(0, 0, 0, 0.30)
+        }
+        .third-col:hover {
+            transform: scale(1.05);
+            box-shadow: 0 0 40px -10px rgba(0, 0, 0, 0.30)
         }
 
         </style>
 
-        <p class="first-col">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis praesentium modi veniam aliquid. Expedita, quae eveniet dignissimos ullam iure dicta! Enim cupiditate iure voluptates odio iste numquam voluptatum consectetur odit!</p>
-        <p class="second-col">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis praesentium modi veniam aliquid. Expedita, quae eveniet dignissimos ullam iure dicta! Enim cupiditate iure voluptates odio iste numquam voluptatum consectetur odit!</p>
-        <p class="third-col">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis praesentium modi veniam aliquid. Expedita, quae eveniet dignissimos ullam iure dicta! Enim cupiditate iure voluptates odio iste numquam voluptatum consectetur odit!</p>
+        <p class="first-col">MEET OUR PLAYER HERE
+                            <img src="./asset/img/squad.jpg"> 
+                            <button><a href="sub_pages/first-team.html">FCUOT First Team Players</a></button>
+        </p>
+        <p class="second-col">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis praesentium modi veniam aliquid. Expedita, quae eveniet dignissimos ullam iure dicta! Enim cupiditate iure voluptates odio iste numquam voluptatum consectetur odit!<br>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis praesentium modi veniam aliquid. Expedita, quae eveniet dignissimos ullam iure dicta! Enim cupiditate iure voluptates odio iste numquam voluptatum consectetur odit!</p>
+        <p class="third-col">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis praesentium modi veniam aliquid. Expedita, quae eveniet dignissimos ullam iure dicta! Enim cupiditate iure voluptates odio iste numquam voluptatum consectetur odit!<br>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis praesentium modi veniam aliquid. Expedita, quae eveniet dignissimos ullam iure dicta! Enim cupiditate iure voluptates odio iste numquam voluptatum consectetur odit!</p>
         `
     }
 }
 
 customElements.define('some-column', SomeColumn);
+
+// const buttplay = document.querySelector('.buttplayer');
+// buttplay.innerHTML = `<h4><a href="sub_pages/first-team.html">Click here to see our players</a></h4>`
